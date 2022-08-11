10039619a054df7ad64e81670787b6b2a1e97bb2
shaima <s.alghamdi@safcsp.org.sa>
2022-08-11 12:59:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/project_2_recyclerview_app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.project_2_recyclerview_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Connect the adapter with the data here
+        // Bind the recyclerview and the adapter here
+    }
+}
